98b4b4cab14c4219abdb01e17160853ccf8a6546
david <jodavidamaya@gmail.com>
2024-06-17 17:32:52 -0500

    se elimino loginexpress

--- loginexpress/validar.js
+++
@@ -1,8 +0,0 @@
-const validar = (req, res) => {
-    if (req.session.usuario) {
-      res.status(200).send('Haz  sido autorizado')
-    } else {
-      res.status(401).send('No eres bienvenido')
-    }
-}
-module.exports = validar
